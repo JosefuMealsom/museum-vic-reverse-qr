ebee0f2c1f9bedf12b6743613307762904258dfe
DMS Host <dmshost@mvms.vic.gov.au>
2024-07-01 12:20:22 +1000

    Add option to set client url for CORS on the server
    Why: so we can host the client on a separate domain

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ import http from "http";
 import { Server } from "socket.io";
 import { throttle } from "underscore";
 
+const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
+const io = new Server(server, { cors: { origin: clientUrl } });
 
 io.on("connection", (client) => {
   const throttledQRDetected = throttle(
