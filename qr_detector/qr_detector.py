d6a69b7b6ae7964a9ce531c09a294910794aad62
DMS Host <dmshost@mvms.vic.gov.au>
2024-07-04 12:23:33 +1000

    Add script to run full application

--- qr_detector/qr_detector.py
+++ qr_detector/qr_detector.py
@@ -13,14 +13,16 @@ def get_resized_dimensions(resized):
 parser = argparse.ArgumentParser("qr_detector")
 parser.add_argument("content_id", help="The content id the camera is linked to", type=int)
 parser.add_argument("--resize_camera_input","-r",  help="Input: {width},{height}. Resizes the camera feed to the specified dimensions", type=get_resized_dimensions)
-parser.add_argument("--socket_server_url","-s",  help="The websocket server to connect to", default='ws://localhost:5000')
-parser.add_argument("--device-index","-d",  help="The usb port the camera is connected to", default=0)
+parser.add_argument("--socket_server_url","-s",  help="The websocket server to connect to", default='wss://server-wandering-fire-1875.fly.dev')
+parser.add_argument("--device-index","-d",  help="The usb port the camera is connected to", default=0, type=int)
+parser.add_argument("--show-window","-w",  help="Whether to hide the open cv output window", action=argparse.BooleanOptionalAction)
 
 args = parser.parse_args()
 content_id = args.content_id
 dimensions = args.resize_camera_input
 socket_server_url = args.socket_server_url
 device_index = args.device_index
+show_window = args.show_window
 
 video_stream = cv2.VideoCapture(device_index)
 
@@ -33,6 +35,8 @@ while True:
     if dimensions != None:
         frame = cv2.resize(frame, dimensions)
 
+    if not success:
+        continue
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #frame = cv2.threshold(frame, 50, 255, cv2.THRESH_BINARY)[1]
 
@@ -52,11 +56,12 @@ while True:
             sio.emit("qr_code_detected", {"sessionID":code.data.decode("ascii"), "contentID": content_id})
     except:
         print("An error occured with the detection")
-
-    cv2.imshow("QR code reader", frame)
-
-    key =  cv2.waitKey(1)
-    if key == 27:
-        break
+    
+    if show_window:
+        cv2.imshow("QR code reader", frame)
+        key =  cv2.waitKey(1)
+        if key == 27:
+            break
+    
 
 video_stream.release()
